b4a15b277d326032699e8e9045cd760698bb5c31
Joe Simpson <me@kennydude.me>
2025-01-18 21:25:02 +0000

    Copy block pages from other journal

--- PublisherPreferenceToolsPageHandler.php
+++ PublisherPreferenceToolsPageHandler.php
@@ -2,10 +2,14 @@
 
 namespace APP\plugins\generic\publisherPreferences;
 
+use APP\core\Application;
 use APP\handler\Handler;
+use PKP\navigationMenu\NavigationMenuItemDAO;
 use APP\template\TemplateManager;
 use PKP\core\JSONMessage;
 use PKP\db\DAORegistry;
+use PKP\navigationMenu\NavigationMenuItem;
+use PKP\security\Role;
 
 class PublisherPreferenceToolsPageHandler extends Handler {
 
@@ -25,13 +29,42 @@ class PublisherPreferenceToolsPageHandler extends Handler {
             return new JSONMessage(false);
         }
 
+        $currentUser = $request->getUser();
         $currentContext = $request->getContext();
+
+        $isAdmin = $currentUser->hasRole([Role::ROLE_ID_MANAGER], $currentContext->getId()) || $currentUser->hasRole([Role::ROLE_ID_SITE_ADMIN], \PKP\core\PKPApplication::SITE_CONTEXT_ID);
+        if(!$isAdmin) {
+            return new JSONMessage(false, 'not admin');
+        }
+
         $copyFromContext = $_POST['journal'];
 
         echo '<div style="display:flex;height:100vh;justify-content:center;align-items:center;flex-direction:column"><h1>Copy Journal Items</h1>';
         
         // Copy here
-        echo '<p>todo: copy journal items</p>';
+        if(class_exists('APP\plugins\generic\blockPages\classes\BlockPage')) {
+            // copy block pages from other journal
+            $blockPagesDao = DAORegistry::getDAO('BlockPagesDAO');
+            $pages = $blockPagesDao->getByContextId($copyFromContext);
+            foreach($pages->toAssociativeArray() as $page) {
+                $page->setContextId($currentContext->getId());
+                $blockPagesDao->insertObject($page);
+                echo '<p>copied page ' . htmlentities($page->getData('title')) . ' across</p>';
+
+                $navMenuItem = new NavigationMenuItem();
+                $navMenuItem->setContextId( $currentContext->getId() );
+                $navMenuItem->setTitle( $page->getData('title'), "en" );
+                $navMenuItem->setType( NavigationMenuItem::NMI_TYPE_REMOTE_URL );
+                $navMenuItem->setPath("");
+                $navMenuItem->setContent("", "en");
+                $navMenuItem->setRemoteUrl( $request->getDispatcher()->url($request, Application::ROUTE_PAGE, $currentContext?->getPath() ?? '', $page->getData('path') ), "en" );
+    
+                $navigationMenuItemDao = DAORegistry::getDAO('NavigationMenuItemDAO'); /** @var NavigationMenuItemDAO $navigationMenuItemDao */
+                $navigationMenuItemDao->insertObject($navMenuItem);
+                echo '<p>created navigation menu item</p>';
+            }
+            echo '<p>copied pages</p>';
+        }
         
         echo '</div>';
 
